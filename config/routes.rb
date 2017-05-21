7a4d0dea2b821017c853ae95d8cfc43d6b5e62a3
Jonathan Santos <santojon5@gmail.com>
2017-05-21 12:04:08 +0000

    home page and bootstrap setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  #homepage
+  get 'welcome/index'
+  root 'welcome#index'
+
+  #application routes
   resources :tokens
   resources :incidents
   resources :users
   resources :equipment
   resources :labs
   resources :desks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
