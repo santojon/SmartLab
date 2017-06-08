de7483a98d7c3c0c5987b21a52a72b251ff2ec15
Jonathan Santos <santojon5@gmail.com>
2017-06-08 04:40:54 +0000

    accesses route and page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :accesses]
 
   # GET /users
   # GET /users.json
@@ -10,6 +10,11 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+  end
+  
+  # GET /users/1/accesses
+  # GET /users/1/accesses.json
+  def accesses
   end
 
   # GET /users/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  #login
+    #login
   devise_for :users
   devise_scope :user do
+    #logout page
     get '/users/sign_out' => 'devise/sessions#destroy'
     
     authenticated :user do
+      #root fot logged users
       root 'home#index'
+      
+      #custom routes
+      get "/users/:id/accesses" => "users#accesses"
       
       #application routes
       resources :tokens
@@ -16,6 +21,7 @@ Rails.application.routes.draw do
       resources :desks
     end
     
+    #home for not logged in (login page)
     root 'devise/sessions#new'
   end
 end
