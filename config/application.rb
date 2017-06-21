58aec3e12d5d56a380e51a2d6ec99acaad43fd70
Jonathan Santos <santojon5@gmail.com>
2017-06-21 02:50:02 +0000

    fix

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Smartlab
   class Application < Rails::Application
-    
-    # Data for all default entities
-    default_users = JSON.parse(File.read("#{Rails.root}/db/bootstrap/users.json"))
-    
-    # Create all default entities from given data
-    default_users.each do |user|
-      User.create(user)
-    end
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
