55cf8da3191d252424ae6af7ea0bf40fedb5a298
Jonathan Santos <santojon5@gmail.com>
2017-06-21 02:45:31 +0000

    fix

--- config/initializers/zboot.rb
+++
@@ -1,7 +0,0 @@
-# Data for all default entities
-default_users = JSON.parse(File.read("#{Rails.root}/db/bootstrap/users.json"))
-
-# Create all default entities from given data
-default_users.each do |user|
-  User.create(user)
-end
