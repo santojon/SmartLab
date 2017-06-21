9b0609e1528a9f156858e16ae462619cf811c3f5
Jonathan Santos <santojon5@gmail.com>
2017-06-21 02:42:32 +0000

    fix

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -1,10 +1,6 @@
 class Desk < ApplicationRecord
 	belongs_to :lab, optional: false
-<<<<<<< HEAD
-	has_many :equipments, :dependent => :delete_all
-=======
 	has_many :equipments, :dependent => :destroy
->>>>>>> master
 
 	has_many :accesses
 	has_many :users, through: :accesses
