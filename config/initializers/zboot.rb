8f541f7cc81a92c8b815fb79db025b2aad266c9a
Jonathan Santos <santojon5@gmail.com>
2017-09-06 16:07:06 +0000

    remove labs creation from zboot

--- config/initializers/zboot.rb
+++ config/initializers/zboot.rb
@@ -1,11 +1,6 @@
 default_users = JSON.parse(File.read("#{Rails.root}/db/bootstrap/users.json"))
-default_labs = JSON.parse(File.read("#{Rails.root}/db/bootstrap/labs.json"))
 
 # Create all default entities from given data
 default_users.each do |user|
   User.create(user)
-end
-
-default_labs.each do |lab|
-  Lab.create(lab)
 end
